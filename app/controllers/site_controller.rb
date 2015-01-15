09a225857fe6a96aa2b4d17059c0dcdf202e6534
Paul Howard <paul@paul-howard.net>
2015-01-14 23:28:28 -0800

    Set up basic user authentication.

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -2,6 +2,7 @@ class SiteController < ApplicationController
 
   def index
     # render plain: "This is the home page. It will display info about the site if no current_user; else will show user's collections/interests."
+    @user = current_user
   end
 
 end
