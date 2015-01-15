43787a1f37319c87888e2c58b2638deb56e7cde0
Paul Howard <paul@paul-howard.net>
2015-01-15 10:36:04 -0800

    Add Collection model; Readme.rdoc >> .md

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ActiveRecord::Base
 
+  has_many :collections
+
   has_secure_password
 
   validates_confirmation_of :password, on: :create
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150115051720) do
+ActiveRecord::Schema.define(version: 20150115074909) do
+
+  create_table "collections", force: :cascade do |t|
+    t.string   "name"
+    t.string   "description"
+    t.integer  "user_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
+  add_index "collections", ["user_id"], name: "index_collections_on_user_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "email"
